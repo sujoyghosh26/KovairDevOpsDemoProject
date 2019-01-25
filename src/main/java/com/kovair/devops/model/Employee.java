67506e683f0bd36e0b837a2672d9625bfef7c45d
prasenjitkovair <prasenjitok@gmail.com>
2019-01-25 17:06:07 +0530

    Updated files committed by Debobrata

---
+++ src/main/java/com/kovair/devops/model/Employee.java
@@ -0,0 +1,54 @@
+package com.kovair.devops.model;
+
+public class Employee {
+	private String firstName;
+	private String lastName;
+	private String designation;
+	private String doj;
+	private String supervisor;
+	private String phone;
+	private String email;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public String getDoj() {
+		return doj;
+	}
+	public void setDoj(String doj) {
+		this.doj = doj;
+	}
+	public String getSupervisor() {
+		return supervisor;
+	}
+	public void setSupervisor(String supervisor) {
+		this.supervisor = supervisor;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
---
+++ src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+<link rel="stylesheet" href="bootstrap/bootstrap.css" type="text/css" />
+<link rel="stylesheet" href="css/LoginPage.css" type="text/css" />
+</head>
+<body>
+<div class="content container-float">
+			<div class="col-sm-6">
+				Welcome To Java Demo Application...
+		</div>
+		<div class="col-sm-6">
+			<a href="login.jsp" id="logoutClick"  class="btn btn-warning btn-md">Log Out</a>
+		</div>
+</div>
+</body>
+</html>
