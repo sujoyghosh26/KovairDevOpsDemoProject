7f617ed2ec136c469f95578e6dac324239f808da
ac289 <mayukhnandy13@gmail.com>
2019-11-27 22:39:39 -0800

    3

--- src/main/java/com/kovair/devops/WebAppInitializer.java
+++ src/main/java/com/kovair/devops/WebAppInitializer.java
@@ -15,14 +15,13 @@ public class WebAppInitializer implements WebApplicationInitializer {
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
 		
-		WebApplicationContext context = getContext();//
+		WebApplicationContext context = getContext();
 		servletContext.addListener(new ContextLoaderListener(context));
 		
 		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(context));
 		dispatcher.setLoadOnStartup(1);
 		dispatcher.addMapping("/");
-		//dispatcher.addMapping("*.jsp");
-		//dispatcher.addMapping("*.html");
+
 		dispatcher.addMapping("*.js");
 		dispatcher.addMapping("*.css");
 		dispatcher.addMapping("*.json");
