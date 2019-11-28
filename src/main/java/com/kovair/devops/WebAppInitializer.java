01acffa513402e01b17e0054d29f109302dc8c2a
ac289 <mayukhnandy13@gmail.com>
2019-11-27 22:58:47 -0800

    3

--- src/main/java/com/kovair/devops/WebAppInitializer.java
+++ src/main/java/com/kovair/devops/WebAppInitializer.java
@@ -21,7 +21,6 @@ public class WebAppInitializer implements WebApplicationInitializer {
 		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(context));
 		dispatcher.setLoadOnStartup(1);
 		dispatcher.addMapping("/");
-
 		dispatcher.addMapping("*.js");
 		dispatcher.addMapping("*.css");
 		dispatcher.addMapping("*.json");
