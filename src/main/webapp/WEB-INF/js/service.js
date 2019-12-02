f21e5527af1b1639ca6518ffae8e4eae54414229
mayukhnandy <mayukhnandy13@gmail.com>
2019-12-02 10:27:39 +0530

    commit - 02122019

--- src/main/webapp/WEB-INF/js/service.js
+++ src/main/webapp/WEB-INF/js/service.js
@@ -85,7 +85,7 @@ devOpsService.factory('devOpsService', function ($http) {
            	        "Department": "Software Engineer"
            	    }
        	];
-
+		$employee = [];
 		return $employee
 	}
 	
