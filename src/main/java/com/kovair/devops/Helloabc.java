b2dae6524d694293ac99d9f98a7858d4e0071045
mayukhnandy <mayukhnandy13@gmail.com>
2019-12-11 00:39:28 -0800

    DP-427

--- src/main/java/com/kovair/devops/Helloabc.java
+++ src/main/java/com/kovair/devops/Helloabc.java
@@ -8,7 +8,7 @@ public class Helloabc {
 
 	public static void main(String[] args) {
 		System.out.println("hiii");
-		System.out.println("hello new 12345");
+		System.out.println("hello new 123456");
 		// TODO Auto-generated method stub
 //new171
 	}
