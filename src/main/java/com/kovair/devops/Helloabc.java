3d571f560c213604d4f72813a76fdc6c1466db2e
sm281 <sm281@demo1.sr.kovair.com>
2019-12-31 01:20:33 -0800

    DP-440

--- src/main/java/com/kovair/devops/Helloabc.java
+++ src/main/java/com/kovair/devops/Helloabc.java
@@ -8,7 +8,7 @@ public class Helloabc {
 
 	public static void main(String[] args) {
 		System.out.println("hiii");
-		System.out.println("hello new abc");
+		System.out.println("hello new abcde");
 		// TODO Auto-generated method stub
 //new171 modified new 123587defabc
 	}
