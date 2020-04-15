1534a17950510150ab815a6dc324dc6a190f14a8
mayukhnandy <mayukhnandy13@gmail.com>
2020-04-15 04:42:45 -0700

    8

--- src/main/java/com/kovair/devops/Helloabc.java
+++ src/main/java/com/kovair/devops/Helloabc.java
@@ -8,7 +8,7 @@ public class Helloabc {
 
 	public static void main(String[] args) {
 		System.out.println("hiii");
-		System.out.println("hello new abcdefgg");
+		System.out.println("hello new abcdefggg");
 		// TODO Auto-generated method stub
 //new171 modified new12
 	}
