9c7b5d9bda97146f0893d9d0fcfdb8dcd2b868c0
mayukhnandy <mayukhnandy13@gmail.com>
2020-09-15 22:39:35 -0700

    233

--- src/main/webapp/WEB-INF/js/service.js
+++ src/main/webapp/WEB-INF/js/service.js
@@ -70,7 +70,7 @@ devOpsService.factory('devOpsService', function ($http) {
            	        "Date of Joining": "21/10/2009",
            	        "Department": "ALM Developement"
            	    },
-           	    //comment added 233
+           	    //comment added 233 mk
            	    {
            	        "First Name": "Soumonil",
            	        "Last Name": "Roy Chowdhury",
